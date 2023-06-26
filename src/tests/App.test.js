6518a17de7fbc76f56de0591429d301d86a91dcf
Jordan <j.r.noble@live.co.uk>
2023-06-26 18:04:22 +0100

    linting added

---
+++ src/tests/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+test('renders heading', () => {
+  render(<App />);
+  const heading = screen.getByText(/Surreal Estate/i);
+  
+  expect(heading).toBeInTheDocument();
+});
